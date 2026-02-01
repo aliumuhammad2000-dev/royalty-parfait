386c5552c985015424cb8627ba57a41a6eb89240
Aliyu Tunde <aliumuhammad2000@gmail.com>
2026-02-01 03:40:43 -0800

    built cart sideview, added js and made it responsive

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ menuBtn.addEventListener('click', () => {
     navMenu.classList.toggle('hidden');
 });
 
+const cartIcon = document.getElementById('cart-icon');
+const cartSide = document.getElementById('cart-side');
+const cartIconClose = document.getElementById('cart-icon-close');
+
+cartIcon.addEventListener('click', () => {
+    cartSide.classList.toggle("hidden");
+});
+cartIconClose.addEventListener('click', () => {
+    cartSide.classList.toggle("hidden");
+});
